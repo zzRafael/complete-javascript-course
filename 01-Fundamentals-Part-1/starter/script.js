c6c6e8fe21e6d342a23aa2b3924254946376cd2d
Rafael Alves <102750561+zzRafael@users.noreply.github.com>
2024-02-27 14:37:39 -0300

    Create script.js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,33 @@
+
+// let userName = 'Rafael'
+// let age = 20;
+// let country = "Brazil"
+
+// console.log(userName)
+// console.log(age)
+// console.log(country)
+
+// userName = 'João'
+// age = 12
+// country = true
+
+// console.log(userName)
+// console.log(age)
+// console.log(country)
+
+// console.log(typeof null)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
